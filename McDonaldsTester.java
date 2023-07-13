61ce417ba3ba631eee5a57e4a177b9c0439d36ef
Suchithraraj29 <suchithra2823@gmail.com>
2023-07-13 14:26:17 +0530

    commiting method_overloading file

---
+++ McDonaldsTester.java
@@ -0,0 +1,138 @@
+ class McDonaldsTester{
+
+  public static void main(String foodDelivery[]){
+  
+  String foodName = "chicken masala";
+  double prizeOfFoodName = McDonalds.search(foodName);
+  System.out.println("The price is " + prizeOfFoodName);
+  double priceWithquantity  = McDonalds.search("chicken masala",3);  
+  System.out.println("the food price with quantity" +priceWithquantity);
+  
+  
+  String foodName1 = "mutton masala";
+  double prizeOfFoodName1 = McDonalds.search(foodName1);
+  System.out.println("The price is " + prizeOfFoodName1);
+  double priceWithquantity1  = McDonalds.search("mutton masala",3);  
+  System.out.println("the food price with quantity" +priceWithquantity1);
+  
+  
+  String foodName2 = "Bheja black pepper dry";
+  double prizeOfFoodName2 = McDonalds.search(foodName2);
+  System.out.println("The price is " + prizeOfFoodName2);
+  double priceWithquantity2  = McDonalds.search("Bheja black pepper dry",3);  
+  System.out.println("the food price with quantity" +priceWithquantity2);
+  
+  
+  String foodName3 = "red chilly fry";
+  double prizeOfFoodName3 = McDonalds.search(foodName3);
+  System.out.println("The price is " + prizeOfFoodName3);
+  double priceWithquantity3  = McDonalds.search("red chilly fry",3); 
+  System.out.println("the food price with quantity" +priceWithquantity3);
+  
+  String foodName4 = "malai paya";
+  double prizeOfFoodName4 = McDonalds.search(foodName4);
+  System.out.println("The price is " + prizeOfFoodName4);
+  double priceWithquantity4 = McDonalds.search("malai paya",3);  
+  System.out.println("the food price with quantity" +priceWithquantity4);
+  
+  String foodName5 = "apollo chicken";
+  double prizeOfFoodName5 = McDonalds.search(foodName5);
+  System.out.println("The price is " + prizeOfFoodName5);
+  double priceWithquantity5 = McDonalds.search("apollo chicken",3);  
+  System.out.println("the food price with quantity" +priceWithquantity5);
+  
+  String foodName6 = "dum ka chicken";
+  double prizeOfFoodName6 = McDonalds.search(foodName6);
+  System.out.println("The price is " + prizeOfFoodName6);
+ double priceWithquantity6 = McDonalds.search("dum ka chicken",3);  
+  System.out.println("the food price with quantity" +priceWithquantity6);
+  
+  String foodName7 = "golden fish";
+  double prizeOfFoodName7 = McDonalds.search(foodName7);
+  System.out.println("The price is " + prizeOfFoodName7);
+  double priceWithquantity7 = McDonalds.search("golden fish",3);  
+  System.out.println("the food price with quantity" +priceWithquantity7);
+  
+  String foodName8 = "egg biryani";
+  double prizeOfFoodName8 = McDonalds.search(foodName8);
+  System.out.println("The price is " + prizeOfFoodName8);
+  double priceWithquantity8 = McDonalds.search("egg biryani",3);  
+  System.out.println("the food price with quantity" +priceWithquantity8);
+  
+  String foodName9 = "desi tandoori chicken";
+  double prizeOfFoodName9 = McDonalds.search(foodName9);
+  System.out.println("The price is " + prizeOfFoodName9);
+ double priceWithquantity9 = McDonalds.search("desi tandoori chicken",3);  
+  System.out.println("the food price with quantity" +priceWithquantity9);
+  
+  
+  String foodName10 = "tandoori roti";
+  double prizeOfFoodName10 = McDonalds.search(foodName10);
+  System.out.println("The price is " + prizeOfFoodName10);
+  double priceWithquantity10 = McDonalds.search("tandoori roti",3);  
+  System.out.println("the food price with quantity" +priceWithquantity10);
+  
+  String foodName11 = "nayaab special mutton";
+  double prizeOfFoodName11 = McDonalds.search(foodName11);
+  System.out.println("The price is " + prizeOfFoodName11);
+  double priceWithquantity11  = McDonalds.search("nayaab special mutton",3);  
+  System.out.println("the food price with quantity" +priceWithquantity11);
+  
+  String foodName12 = "mutton talahua";
+  double prizeOfFoodName12 = McDonalds.search(foodName12);
+  System.out.println("The price is " + prizeOfFoodName12);
+  double priceWithquantity12 = McDonalds.search("mutton talahua",3);  
+  System.out.println("the food price with quantity" +priceWithquantity12);
+  
+  String foodName13 = "nayaab special chicken";
+  double prizeOfFoodName13 = McDonalds.search(foodName13);
+  System.out.println("The price is " + prizeOfFoodName13);
+  double priceWithquantity13 = McDonalds.search("nayaab special chicken",3);  
+  System.out.println("The price is " + prizeOfFoodName13);
+  
+  
+  String foodName14 = "chicken talahua";
+  double prizeOfFoodName14 = McDonalds.search(foodName14);
+  System.out.println("The price is " + prizeOfFoodName14);
+  double priceWithquantity14 = McDonalds.search("chicken talahua",3);  
+  System.out.println("the food price with quantity" +priceWithquantity14);
+  
+  String foodName15 = "veg biryani";
+  double prizeOfFoodName15 = McDonalds.search(foodName15);
+  System.out.println("The price is " + prizeOfFoodName15);
+  double priceWithquantity15= McDonalds.search("veg biryani",3);  
+  System.out.println("the food price with quantity" +priceWithquantity15);
+  
+  String foodName16 = "chicken manchurian";
+  double prizeOfFoodName16 = McDonalds.search(foodName16);
+  System.out.println("The price is " + prizeOfFoodName16);
+  double priceWithquantity16= McDonalds.search("chicken manchurian",3);  
+  System.out.println("the food price with quantity" +priceWithquantity16);
+  
+  String foodName17 = "sweet chilly chicken";
+  double prizeOfFoodName17 = McDonalds.search(foodName17);
+  System.out.println("The price is " + prizeOfFoodName17);
+  double priceWithquantity17= McDonalds.search("sweet chilly chicken",3);  
+  System.out.println("the food price with quantity" +priceWithquantity17);
+  
+  
+  String foodName18 = "ginger chicken";
+  double prizeOfFoodName18 = McDonalds.search(foodName18);
+  System.out.println("The price is " + prizeOfFoodName18);
+  double priceWithquantity18= McDonalds.search("ginger chicken",3);  
+  System.out.println("the food price with quantity" +priceWithquantity18);
+  
+  String foodName19 = "arabi tandoori chicken";
+  double prizeOfFoodName19 = McDonalds.search(foodName19);
+  System.out.println("The price is " + prizeOfFoodName19);
+  double priceWithquantity19= McDonalds.search("arabi tandoori chicken",3);  
+  System.out.println("the food price with quantity" +priceWithquantity19);
+  
+  String foodName20 = "fish 65";
+  double prizeOfFoodName20 = McDonalds.search(foodName20);
+  System.out.println("The price is " + prizeOfFoodName20);
+  double priceWithquantity20= McDonalds.search("fish 65",3);  
+  System.out.println("the food price with quantity" +priceWithquantity20);
+  
+  }
+}
---
+++ Swiggy1Tester.java
@@ -0,0 +1,146 @@
+class Swiggy1Tester{
+
+  public static void main(String foodDelivery[]){
+  
+  String foodName = "Coke";
+  double priceOfFoodName = Swiggy.search(foodName);
+  System.out.println("The price is " + priceOfFoodName);
+  double priceWithquantity  = Swiggy1.search("Coke",3);  
+  System.out.println("the food price with quantity" +priceWithquantity);
+  
+  String foodName1 = "Pepsi";
+  double priceOfFoodName1 = Swiggy1.search(foodName1);
+  System.out.println("The price is " + priceOfFoodName1);
+  double priceWithquantity1 = Swiggy1.search("Pepsi",2);  
+  System.out.println("the food price With quantity" +priceWithquantity1);
+  
+  String foodName2 = "Potato twister";
+  double priceOfFoodName2 = Swiggy1.search(foodName2);
+   System.out.println("The price is " + priceOfFoodName2);
+   double priceWithquantity2  = Swiggy1.search("patato twister",3);  
+  System.out.println("the food price with quantity" +priceWithquantity2);
+  
+  String foodName3 = "Nuggets";
+  double priceOfFoodName3 = Swiggy1.search(foodName3);
+  System.out.println("The price is " + priceOfFoodName3);
+  double priceWithquantity3  = Swiggy1.search("Nuggets",3);  
+  System.out.println("the food price with quantity" +priceWithquantity3);
+  
+  String foodName4 = "Chicken legs";
+  double priceOfFoodName4 = Swiggy1.search(foodName4);
+  System.out.println("The price is " + priceOfFoodName4);
+    double priceWithquantity4  = Swiggy1.search("Chicken legs",5);  
+System.out.println("the food price with quantity" +priceWithquantity4);
+  
+  String foodName5 = "Kashmiri kheer";
+  double priceOfFoodName5 = Swiggy1.search(foodName5);
+  System.out.println("The price is " + priceOfFoodName5);
+      double priceWithquantity5  = Swiggy1.search("Kashmiri kheer",3);  
+  System.out.println("the food price with quantity" +priceWithquantity5);
+  
+  String foodName6 = "chicken mini";
+  double priceOfFoodName6 = Swiggy1.search(foodName6);
+  System.out.println("The price is " + priceOfFoodName6);
+  double priceWithquantity6= Swiggy1.search("chicken mini",5);  
+  System.out.println("the food price with quantity" +priceWithquantity6);
+  
+  String foodName7 = "grilled chicken";
+  double priceOfFoodName7 = Swiggy1.search(foodName7);
+  System.out.println("The price is " + priceOfFoodName7);
+  double priceWithquantity7  = Swiggy1.search("grilled chicken",3);  
+  System.out.println("the food price with quantity" +priceWithquantity7);
+  
+  
+  String foodName8 = "tandoori chicken";
+  double priceOfFoodName8 = Swiggy1.search(foodName8);
+  System.out.println("The price is " + priceOfFoodName8);
+  double priceWithquantity8  = Swiggy1.search("tandoori chicken",3);  
+  System.out.println("the food price with quantity" +priceWithquantity8);
+  
+  
+  String foodName9 = "chicken fried rice";
+  double priceOfFoodName9 = Swiggy1.search(foodName9);
+  System.out.println("The price is " + priceOfFoodName9);
+  double priceWithquantity9  = Swiggy1.search("chicken fried rice",6);  
+  System.out.println("the food price with quantity" + priceWithquantity9);
+  
+  
+  String foodName10 = "chicken jumbo pack";
+  double priceOfFoodName10 = Swiggy1.search(foodName10);
+  System.out.println("The price is " + priceOfFoodName10);
+ double priceWithquantity10  = Swiggy1.search("chicken jumbo pack",3);  
+  System.out.println("the food price with quantity" +priceWithquantity10);
+  
+  
+  String foodName11 = "chicken 65 dry";
+  double priceOfFoodName11 = Swiggy1.search(foodName11);
+  System.out.println("The price is " + priceOfFoodName11);
+  double priceWithquantity11 = Swiggy1.search("chicken 65 dry",3);  
+  System.out.println("the food price with quantity" +priceWithquantity11);
+  
+  
+  String foodName12 = "special chicken boneless biryani";
+  double priceOfFoodName12 = Swiggy1.search(foodName12);
+  System.out.println("The price is " + priceOfFoodName12);
+  double priceWithquantity12  = Swiggy1.search(" special chicken boneless beryani ",3);  
+  System.out.println("the food price with quantity" +priceWithquantity12);
+  
+  
+  String foodName13 = "mutton jumbo pack";
+  double priceOfFoodName13 = Swiggy1.search(foodName13);
+  System.out.println("The price is " + priceOfFoodName13);
+  double priceWithquantity13  = Swiggy1.search("mutton jumbo pack",3);  
+  System.out.println("the food price with quantity" +priceWithquantity13);
+  
+  
+  String foodName14 = "chicken noodles";
+  double priceOfFoodName14 = Swiggy1.search(foodName14);
+  System.out.println("The price is " + priceOfFoodName14);
+  double priceWithquantity14  = Swiggy1.search("chicken noodles",3);  
+  System.out.println("the food price with quantity" +priceWithquantity14);
+  
+  
+  String foodName15 = "tangdi kebab";
+  double priceOfFoodName15 = Swiggy1.search(foodName15);
+  System.out.println("The price is " + priceOfFoodName15);
+   double priceWithquantity15  = Swiggy1.search("tangdi kebab",3);  
+  System.out.println("the food price with quantity" +priceWithquantity15);
+  
+  
+  String foodName16 = "chicken tikka kebab";
+  double priceOfFoodName16 = Swiggy1.search(foodName16);
+  System.out.println("The price is " + priceOfFoodName16);
+  double priceWithquantity16  = Swiggy1.search("chicken tikka kebab",3);  
+  System.out.println("the food price with quantity" + priceWithquantity16);
+  
+  
+  String foodName17 = "butter chicken";
+  double priceOfFoodName17 = Swiggy1.search(foodName17);
+  System.out.println("The price is " + priceOfFoodName17);
+ double priceWithquantity17  = Swiggy1.search("butter chicken",3);  
+  System.out.println("the food price with quantity" +priceWithquantity17);
+  
+  
+  String foodName18 = "chilly chicken";
+  double priceOfFoodName18 = Swiggy1.search(foodName18);
+  System.out.println("The price is " + priceOfFoodName18);
+double priceWithquantity18  = Swiggy1.search("chilly chicken",3);  
+  System.out.println("the food price with quantity" +priceWithquantity18);
+  
+  
+  String foodName19 = "coffee";
+  double priceOfFoodName19 = Swiggy1.search(foodName19);
+  System.out.println("The price is " + priceOfFoodName19);
+  double priceWithquantity19  = Swiggy1.search("coffee",3);  
+  System.out.println("the food price with quantity" +priceWithquantity19);
+  
+  
+  String foodName20 = "mutton family pack";
+  double priceOfFoodName20 = Swiggy1.search(foodName20);
+  System.out.println("The price is " + priceOfFoodName20);
+ double priceWithquantity20  = Swiggy1.search("mutton family pack",3);  
+  System.out.println("the food price with quantity" +priceWithquantity20);
+  
+  
+  }
+}
